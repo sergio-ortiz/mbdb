d61158b64b87f764c9b981cdd51f9983af1740f2
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-13 10:43:05 -0400

    feat(next): link navigation through prisma paginationw

--- pages/[[...page]].js
+++ pages/[[...page]].js
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma";
+import Link from "next/link";
 
 export async function getServerSideProps({ params }) {
   const page = params.page ? params.page.shift() : 1;
@@ -10,48 +11,58 @@ export async function getServerSideProps({ params }) {
 
   const json = JSON.stringify(results);
 
-  const props = { json };
+  const props = { page, json };
 
   return { props };
 }
 
-const HomePage = ({ json }) => (
-  <table>
-    <thead>
-      <tr>
-        <th>name</th>
-        <th>address</th>
-        <th>city</th>
-        <th>state</th>
-        <th>zip</th>
-        <th>county</th>
-        <th>minority</th>
-        <th>contact</th>
-        <th>phone</th>
-        <th>email</th>
-        <th>website</th>
-        <th>naics_codes</th>
-      </tr>
-    </thead>
-    <tbody>
-      {JSON.parse(json).map((biz) => (
+const HomePage = ({ page, json }) => (
+  <>
+    <Link href={`/${parseInt(page) - 1}`}>
+      <a>prev</a>
+    </Link>
+    <br />
+    <Link href={`/${parseInt(page) + 1}`}>
+      <a>next</a>
+    </Link>
+
+    <table>
+      <thead>
         <tr>
-          <td>{biz.name}</td>
-          <td>{biz.address}</td>
-          <td>{biz.city}</td>
-          <td>{biz.state}</td>
-          <td>{biz.zip}</td>
-          <td>{biz.county}</td>
-          <td>{biz.minority}</td>
-          <td>{biz.contact}</td>
-          <td>{biz.phone}</td>
-          <td>{biz.email}</td>
-          <td>{biz.website}</td>
-          <td>{biz.naics_codes}</td>
+          <th>name</th>
+          <th>address</th>
+          <th>city</th>
+          <th>state</th>
+          <th>zip</th>
+          <th>county</th>
+          <th>minority</th>
+          <th>contact</th>
+          <th>phone</th>
+          <th>email</th>
+          <th>website</th>
+          <th>naics_codes</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {JSON.parse(json).map((biz) => (
+          <tr>
+            <td>{biz.name}</td>
+            <td>{biz.address}</td>
+            <td>{biz.city}</td>
+            <td>{biz.state}</td>
+            <td>{biz.zip}</td>
+            <td>{biz.county}</td>
+            <td>{biz.minority}</td>
+            <td>{biz.contact}</td>
+            <td>{biz.phone}</td>
+            <td>{biz.email}</td>
+            <td>{biz.website}</td>
+            <td>{biz.naics_codes}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </>
 );
 
 export default HomePage;
