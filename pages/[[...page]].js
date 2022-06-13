87d2e87e6e6bf447f1240dd1d19dc4aa95bc09b0
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-13 10:14:48 -0400

    feat(prisma): pagination

--- pages/[[...page]].js
+++ pages/[[...page]].js
@@ -1,7 +1,13 @@
 import prisma from "../lib/prisma";
 
-export async function getServerSideProps() {
-  const results = await prisma.business.findMany({ take: 10 });
+export async function getServerSideProps({ params }) {
+  const page = params.page ? params.page.shift() : 1;
+
+  const results = await prisma.business.findMany({
+    skip: page * 10 - 10,
+    take: 10,
+  });
+
   const json = JSON.stringify(results);
 
   const props = { json };
