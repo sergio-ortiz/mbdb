38dfb7bdfa4d8f2ae67655cbe1d26d2a8fbcf686
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-13 03:36:14 -0400

    feat(home): page to render data in table format, first ten rows

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import prisma from "../lib/prisma";
 
 export async function getServerSideProps() {
-  const results = await prisma.business.findMany();
+  const results = await prisma.business.findMany({ take: 10 });
   const json = JSON.stringify(results);
 
   const props = { json };
@@ -9,6 +9,43 @@ export async function getServerSideProps() {
   return { props };
 }
 
-const HomePage = ({ json }) => <p>{json}</p>;
+const HomePage = ({ json }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>name</th>
+        <th>address</th>
+        <th>city</th>
+        <th>state</th>
+        <th>zip</th>
+        <th>county</th>
+        <th>minority</th>
+        <th>contact</th>
+        <th>phone</th>
+        <th>email</th>
+        <th>website</th>
+        <th>naics_codes</th>
+      </tr>
+    </thead>
+    <tbody>
+      {JSON.parse(json).map((biz) => (
+        <tr>
+          <td>{biz.name}</td>
+          <td>{biz.address}</td>
+          <td>{biz.city}</td>
+          <td>{biz.state}</td>
+          <td>{biz.zip}</td>
+          <td>{biz.county}</td>
+          <td>{biz.minority}</td>
+          <td>{biz.contact}</td>
+          <td>{biz.phone}</td>
+          <td>{biz.email}</td>
+          <td>{biz.website}</td>
+          <td>{biz.naics_codes}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 
 export default HomePage;
