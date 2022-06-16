9555f14ff19a9ff868e9a492bd4e79a123c9595e
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-16 14:43:56 -0400

    fix(routing): replace a tags on table rows with next router instead

--- components/table.js
+++ components/table.js
@@ -1,34 +1,34 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Table = ({ json }) => (
-  <table className="container">
-    <thead>
-      <tr>
-        <th>name</th>
-        <th>address</th>
-        <th>city</th>
-        <th>zip</th>
-        <th>county</th>
-        <th>minority</th>
-      </tr>
-    </thead>
-    <tbody>
-      {JSON.parse(json).map((biz) => (
-        <Link href={`/business/${biz.id}`} key={biz.id}>
-          <a>
-            <tr>
-              <td>{biz.name}</td>
-              <td>{biz.address}</td>
-              <td>{biz.city}</td>
-              <td>{biz.zip}</td>
-              <td>{biz.county}</td>
-              <td>{biz.minority}</td>
-            </tr>
-          </a>
-        </Link>
-      ))}
-    </tbody>
-  </table>
-);
+const Table = ({ json }) => {
+  const router = useRouter();
+
+  return (
+    <table className="container">
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>address</th>
+          <th>city</th>
+          <th>zip</th>
+          <th>county</th>
+          <th>minority</th>
+        </tr>
+      </thead>
+      <tbody>
+        {JSON.parse(json).map((biz) => (
+          <tr key={biz.id} onClick={() => router.push(`/business/${biz.id}`)}>
+            <td>{biz.name}</td>
+            <td>{biz.address}</td>
+            <td>{biz.city}</td>
+            <td>{biz.zip}</td>
+            <td>{biz.county}</td>
+            <td>{biz.minority}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
