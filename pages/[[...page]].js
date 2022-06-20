e10205565b50c22c0f3facf3c75249e014472823
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-20 14:32:57 -0400

    feat(index): page search results based on search-bar route paramaters

--- pages/[[...page]].js
+++ pages/[[...page]].js
@@ -5,10 +5,12 @@ import Table from "../components/table";
 export async function getServerSideProps({ params }) {
   let page = 1;
   let search = null;
+  let filter = {};
 
   if (params.page) {
     if (params.page.length > 1) {
       search = params.page.pop();
+      filter = { name: { startsWith: search } };
     }
     page = params.page.shift();
   }
@@ -16,6 +18,7 @@ export async function getServerSideProps({ params }) {
   const results = await prisma.business.findMany({
     skip: page * 10 - 10,
     take: 10,
+    where: filter,
   });
 
   const json = JSON.stringify(results);
